81754112347609c091fb0a2a55972ec014ee0086
Regis Teixeira <regismarcondes2016@gmail.com>
2022-04-27 22:37:22 -0300

    Criando lógica de calcular

--- app/src/main/java/com/ds/tiptime/MainActivity.kt
+++ app/src/main/java/com/ds/tiptime/MainActivity.kt
@@ -2,10 +2,25 @@ package com.ds.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ds.tiptime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.botaoCalcular.setOnClickListener{ calcularGorjeta()}
+    }
+    fun calcularGorjeta(){
+        val stringInTextFIeld = binding.custoServico.text.toString()
+        val custo = stringInTextFIeld.toDouble()
+        val selectedId = binding.opcoes.checkedRadioButtonId
+        val gorjetaPorcentagem = when (selectedId) {
+            R.id.opcao_vinte -> 0.20
+            R.id.opcao_dezoito -> 0.18
+            else -> 0.15
+        }
+        var gorjeta = gorjetaPorcentagem * custo
     }
 }
